c0b916b794de5a313a5eb49bc1606bdc04db524b
ctcthrn <79452448+ctcthrn@users.noreply.github.com>
2021-02-22 15:43:52 +0300

    Update razryad.cpp

--- razryad.cpp
+++ razryad.cpp
@@ -3,15 +3,15 @@ int main(){
 	int x,xx,a,b,k;
 	int n = 10, nn = 1;
 	std::cin>>x;
-	while ((x/n)!=0){
-		n = n*10;
-		nn = nn + 1;
-	}
-	std::cout<<"number of digits - "<<nn<<std::endl;
-int nz=n;
+while ((x/n)!=0){
+	n = n*10;
+	nn = nn + 1;
+}
+std::cout<<"number of digits - "<<nn<<std::endl;
+	int nz=n;
 for(k=1;k<nn+1;k++){
 	xx = x/(n/10);
-std::cout<<xx<<"*"<<n/10<<std::endl;
+	std::cout<<xx<<"*"<<n/10<<std::endl;
 	x = x%(n/10);
 	n = n/10;
 } 
