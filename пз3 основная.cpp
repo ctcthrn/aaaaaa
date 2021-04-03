9ca74bcef72d83e7a90cda767077823f7cf90d19
ctcthrn <79452448+ctcthrn@users.noreply.github.com>
2021-04-03 15:37:29 +0300

    Add files via upload

---
+++ пз3 основная.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(){
+	
+	int Big[99999];
+	int n;
+	int Help;
+	
+	cin>>n;
+	
+	for (int k=0; k<n ; k++){
+		Big[k]=rand()%10;
+		cout<<Big[k]<<" ";
+	}
+	
+	cout<<" "<<endl;
+	
+	for (int i=0; i<n; i++){
+		for (int j=0; j<n-i; j++){
+			if (Big[j]>Big[j+1]){
+					Help=Big[j+1];
+					Big[j+1]=Big[j];
+					Big[j]=Help;
+			}
+			else {
+				for (int j=n; j<n-i; j--){
+					if (Big[j]>Big[j-1]){
+						Help=Big[j-1];
+						Big[j-1]=Big[j];
+						Big[j]=Help;
+					}
+				}
+			}
+		}
+	}
+	for (int k=0; k<n ; k++){
+		cout<<Big[k]<<" ";
+	}	
+}
