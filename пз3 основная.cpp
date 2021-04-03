b4a8cc27bccabbe8f85d4f632ce55189a30b950d
ctcthrn <79452448+ctcthrn@users.noreply.github.com>
2021-04-03 16:22:38 +0300

    Update пз3 основная.cpp

--- пз3 основная.cpp
+++ пз3 основная.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 	
 	int Big[99999];
-	int n;
+	int n,s1,s2;
 	int Help;
 	
 	cin>>n;
@@ -19,23 +19,27 @@ int main(){
 	for (int i=0; i<n-1; i++){
 		for (int j=0; j<n-1-i; j++){
 			if (Big[j]>Big[j+1]){
-					Help=Big[j+1];
-					Big[j+1]=Big[j];
-					Big[j]=Help;
-			}
-
-			for (int j=n; j<n-1-i; j--){
-				if (Big[j]>Big[j-1]){
-					Help=Big[j-1];
-					Big[j-1]=Big[j];
-					Big[j]=Help;
-				}
+				Help=Big[j+1];
+				Big[j+1]=Big[j];
+				Big[j]=Help;
+				s1++;	
 			}
 		}
+		for (int j=n-1-i; j>0; j--){
+			if (Big[j]<Big[j-1]){
+				Help=Big[j-1];
+				Big[j-1]=Big[j];
+				Big[j]=Help;
+				s2++;
+				}
+			}
 	}
 	
 	for (int k=0; k<n ; k++){
 		cout<<Big[k]<<" ";
 	}	
-}
 	
+	cout<<" "<<endl;
+	cout<<"1 cycle "<<s1<<endl;
+	cout<<"2 cycle "<<s2;
+}
