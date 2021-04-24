cbe4d7281e5f22bb6dc157f13f3c4e874c7ec479
ctcthrn <79452448+ctcthrn@users.noreply.github.com>
2021-04-24 09:42:32 +0300

    Update пз2 основная.cpp

--- пз2 основная.cpp
+++ пз2 основная.cpp
@@ -1,17 +1,18 @@
-#include <iostream> <cctype>
-	int main(){
+#include <iostream>
+using namespace std;
+int main(){
 	char num;
 	int sys, res;
-	std::cin>>sys>>num;	
+	cin>>sys>>num;	
 	res=0;
-	while ((num = std::cin.get())!=' ') {
+	while(num!=';'){
 		res*=sys;
-		if (num-'0'>9){
-			res+=num-'A'+10;
-		} else	
-			res+=num - '0';
+			if (num-'0'>9){
+				res+=num-'A'+10;
+			} else	
+				res+=num - '0';
+		cin>>num;
 	}
-		std::cout<<res;	
+	cout<<res;	
 }
-
 
